f2f4b3beb03b9621dd31ff3a59a9418e8bbebf64
Mrinal Rai <mrynalrai@gmail.com>
2025-03-01 22:08:43 -0500

    test: using Setup

--- src/Sparky/Customer.cs
+++ src/Sparky/Customer.cs
@@ -2,9 +2,11 @@ namespace Sparky
 {
     public class Customer
     {
+        public string? Greeting { get; set; }
         public string GreetCustomer(string firstName, string lastName)
         {
-            return $"Bonjour {firstName} {lastName}";
+            Greeting = $"Bonjour {firstName} {lastName}";
+            return Greeting;
         }
     }
 }
--- test/SparkyNUnitTest/CustomerTests.cs
+++ test/SparkyNUnitTest/CustomerTests.cs
@@ -6,17 +6,32 @@ namespace SparkyNUnitTest
     [TestFixture]
     public class CustomerTests
     {
+        public required Customer customer;
+        [SetUp]
+        public void Init()
+        {
+            // Arrange
+            customer = new Customer();
+        }
+        
         [Test]
         [TestCase("Jon", "Snow")]
         [TestCase("Sansa", "Stark")]
         public void GreetCustomer_InputNames_ReturnCorrectGreeting(string firstName, string lastName)
         {
-            Customer customer = new ();
-            string result = customer.GreetCustomer(firstName, lastName);
-            Assert.That(result, Does.Contain(firstName));
-            Assert.That(result, Does.Contain(lastName));
-            Assert.That(result, Does.StartWith("Bonjour"));
-            Assert.That(result, Does.EndWith(lastName));
+            // Act
+            customer.GreetCustomer(firstName, lastName);
+            // Assert
+            Assert.That(customer.Greeting, Does.Contain(firstName));
+            Assert.That(customer.Greeting, Does.Contain(lastName));
+            Assert.That(customer.Greeting, Does.StartWith("Bonjour"));
+            Assert.That(customer.Greeting, Does.EndWith(lastName));
+        }
+
+        [Test]
+        public void GreetCustomer_InputNoNames_ReturnNullGreeting()
+        {
+            Assert.That(customer.Greeting, Is.Null);
         }
     }
 }
