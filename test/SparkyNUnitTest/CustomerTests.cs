82d4cf059844183cb009f36507dbbb4725ef9e54
Mrinal Rai <mrynalrai@gmail.com>
2025-03-01 19:40:46 -0500

    test: using String Assert Helper Methods

--- test/SparkyNUnitTest/CustomerTests.cs
+++ test/SparkyNUnitTest/CustomerTests.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using NUnit.Framework;
 using Sparky;
 
@@ -11,13 +7,16 @@ namespace SparkyNUnitTest
     public class CustomerTests
     {
         [Test]
-        [TestCase("Jon", "Snow", ExpectedResult = "Bonjour Jon Snow")]
-        [TestCase("Sansa", "Stark", ExpectedResult = "Bonjour Sansa Stark")]
-        public string GreetCustomer_InputNames_ReturnCorrectGreeting(string firstName, string lastName)
+        [TestCase("Jon", "Snow")]
+        [TestCase("Sansa", "Stark")]
+        public void GreetCustomer_InputNames_ReturnCorrectGreeting(string firstName, string lastName)
         {
             Customer customer = new ();
             string result = customer.GreetCustomer(firstName, lastName);
-            return result;
+            Assert.That(result, Does.Contain(firstName));
+            Assert.That(result, Does.Contain(lastName));
+            Assert.That(result, Does.StartWith("Bonjour"));
+            Assert.That(result, Does.EndWith(lastName));
         }
     }
 }
